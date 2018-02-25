ff8e657f0b5ac9e9e9565305afa4daad1b5214d7
wg-rolf <rolf.neuberger@wheregroup.com>
2018-02-26 00:55:29 +0100

    [NFC] added type annotation, fixed docs

--- src/OwsProxy3/CoreBundle/Controller/OwsProxyController.php
+++ src/OwsProxy3/CoreBundle/Controller/OwsProxyController.php
@@ -1,7 +1,7 @@
 <?php
 namespace OwsProxy3\CoreBundle\Controller;
 
-use Buzz\Message\MessageInterface;
+use Mapbender\CoreBundle\Component\Signer;
 use OwsProxy3\CoreBundle\Component\Utils;
 use OwsProxy3\CoreBundle\Component\CommonProxy;
 use OwsProxy3\CoreBundle\Component\Exception\HTTPStatus403Exception;
@@ -9,12 +9,13 @@ use OwsProxy3\CoreBundle\Component\Exception\HTTPStatus502Exception;
 use OwsProxy3\CoreBundle\Component\ProxyQuery;
 use OwsProxy3\CoreBundle\Component\WmsProxy;
 use OwsProxy3\CoreBundle\Component\WfsProxy;
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use ArsGeografica\Signing\Signer;
 
 //use OwsProxy3\CoreBundle\Component\Url;
 
@@ -26,19 +27,21 @@ use ArsGeografica\Signing\Signer;
  */
 class OwsProxyController extends Controller
 {
+    /** @var LoggerInterface */
     protected $logger = null;
 
     /**
      * Handles the client's request
      *
-     * @param type $url the url
-     * @param type $content the POST content
+     * @param string $url the url
+     * @param string $content the POST content
      * @return \Symfony\Component\HttpFoundation\Response the response
      */
     public function genericProxyAction($url, $content = null)
     {
         $this->container->get('session')->save();
         $this->logger = $this->container->get('logger');
+        /** @var Request $request */
         $request = $this->get('request');
         try {
             $this->logger->debug("OwsProxyController->genericProxyAction");
@@ -58,7 +61,7 @@ class OwsProxyController extends Controller
                 $postParams,
                 $content
             );
-            $proxy = new CommonProxy($proxy_config, $proxy_query, $this->get('logger'));
+            $proxy = new CommonProxy($proxy_config, $proxy_query, $this->logger);
             $cookies_req = $request->cookies;
             $response = new Response();
             $browserResponse = $proxy->handle();
@@ -94,7 +97,9 @@ class OwsProxyController extends Controller
     {
         $this->container->get('session')->save();
         $this->logger = $this->container->get('logger');
+        /** @var Request $request */
         $request = $this->get('request');
+        /** @var Signer $signer */
         $signer = $this->get('signer');
         $proxy_query = ProxyQuery::createFromRequest($request);
         try {
@@ -110,6 +115,7 @@ class OwsProxyController extends Controller
             case 'WMS':
                 try {
                     $this->logger->debug("OwsProxyController->entryPointAction");
+                    /** @var EventDispatcherInterface $dispatcher */
                     $dispatcher = $this->container->get('event_dispatcher');
                     $proxy_config = $this->container->getParameter("owsproxy.proxy");
                     $proxy = new WmsProxy($dispatcher, $proxy_config, $proxy_query, $this->logger);
@@ -187,7 +193,7 @@ class OwsProxyController extends Controller
     }
 
     /**
-     * Creates a response with an exception as png image
+     * Creates a response with an exception as HTML
      *
      * @param \Exception $e the exception
      * @param Request $request the request
