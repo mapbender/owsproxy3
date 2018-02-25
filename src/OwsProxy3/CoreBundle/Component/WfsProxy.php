f09d368b54e6516883b1ef468d24e4394988c035
wg-rolf <rolf.neuberger@wheregroup.com>
2018-02-25 23:13:39 +0100

    [NFC] remove unreachable code

--- src/OwsProxy3/CoreBundle/Component/WfsProxy.php
+++ src/OwsProxy3/CoreBundle/Component/WfsProxy.php
@@ -54,48 +54,6 @@ class WfsProxy extends CommonProxy
         } else {
             throw new HTTPStatus502Exception();
         }
-
-        $response = new Response();
-        $browser = new Browser();
-
-        $dispatcher = $this->container->get('event_dispatcher');
-
-        try {
-            $event = new BeforeProxyEvent($url);
-            $dispatcher->dispatch('owsproxy.before_proxy', $event);
-        } catch (\RuntimeException $e) {
-            return;
-        }
-
-        $incoming = $this->container->get('request');
-        $body = $incoming->getContent();
-        $headers = array_map(function($value) {
-            return implode('; ', $value);
-        }, $incoming->headers->all());
-
-        $headers['User-Agent'] = $this->userAgent;
-        $browserResponse = $browser->post($url->toString(), $headers, $body);
-
-        if ($browserResponse->isOk()) {
-            $event = new AfterProxyEvent($url, $browserResponse);
-            $dispatcher->dispatch('owsproxy.after_proxy', $event);
-
-            // Set received headers to our response
-            foreach ($browserResponse->getHeaders() as $header) {
-                if (strstr($header, ":") === false)
-                    continue;
-
-                list($key, $val) = explode(":", $header, 2);
-                //$response->headers->set($key, $val);
-            }
-
-            // Set received content to our response
-            $response->setContent($browserResponse->getContent());
-        } else {
-            throw new HTTPStatus502Exception();
-        }
-
-        return $response;
     }
 
 }
