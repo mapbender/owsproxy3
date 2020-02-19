a615f12fd46ed85b3a3ddf7537b285beda3991c3
werrolf <rolf.neuberger@wheregroup.com>
2020-02-19 23:58:46 +0100

    Remove explicitly customizable header blacklist and whitelist and user agent from CommonProxy

--- src/OwsProxy3/CoreBundle/Component/CommonProxy.php
+++ src/OwsProxy3/CoreBundle/Component/CommonProxy.php
@@ -15,39 +15,18 @@ class CommonProxy extends BuzzClientCommon
     /** @var ProxyQuery */
     protected $proxy_query;
 
-    /** @var array headernames */
-    protected $headerBlackList = array("cookie", "user-agent", "content-length", "referer", "host");
-
-    /** @var array headernames */
-    protected $headerWhiteList = array();
-
     /**
      * Creates a common proxy
      *
      * @param array $proxy_config
      * @param ProxyQuery $proxy_query
      * @param LoggerInterface|null $logger
-     * @param string[]|null $headerBlackList omit for defaults
-     * @param string[]|null $headerWhiteList omit for defaults
-     * @param string $userAgent
-     * @todo v3.2: remove user agent argument
-     * @todo v3.2: remove header blacklist / whitelist arguments
      */
-    public function __construct(array $proxy_config, ProxyQuery $proxy_query, $logger = null, $headerBlackList = null,
-        $headerWhiteList = null, $userAgent = 'OWSProxy3')
+    public function __construct(array $proxy_config, ProxyQuery $proxy_query, $logger = null)
     {
         @trigger_error("Deprecated: CommonProxy is deprecated since v3.1.6 and will be removed in v3.3. Use owsproxy.buzz_client service instead", E_USER_DEPRECATED);
-        if (func_num_args() >= 4) {
-            @trigger_error("Deprecated: constructor arguments headerBlackList, headerWhiteList and userAgent are deprecated since v3.1.6 and will be ignored in v3.2", E_USER_DEPRECATED);
-        }
-        parent::__construct($proxy_config, $userAgent, $logger);
+        parent::__construct($proxy_config, null, $logger);
         $this->proxy_query = $proxy_query;
-        if ($headerBlackList !== null) {
-            $this->headerBlackList = $headerBlackList;
-        }
-        if ($headerWhiteList !== null) {
-            $this->headerWhiteList = $headerWhiteList;
-        }
     }
 
     /**
@@ -58,15 +37,11 @@ class CommonProxy extends BuzzClientCommon
      */
     public function handle()
     {
-        $headers = Utils::prepareHeadersForRequest($this->proxy_query->getHeaders(), $this->headerBlackList,
-            $this->headerWhiteList);
-        $headers['User-Agent'] = $this->getUserAgent();
-
         $this->logger->debug("CommonProxy->handle {$this->proxy_query->getMethod()}", array(
             'url' => $this->proxy_query->getUrl(),
-            'headers' => $headers,
+            'headers' => $this->proxy_query->getHeaders(),
         ));
-        return $this->handleQueryInternal($this->proxy_query, $this->proxyParams, $headers);
+        return $this->handleQueryInternal($this->proxy_query, $this->proxyParams);
     }
 
     /**
