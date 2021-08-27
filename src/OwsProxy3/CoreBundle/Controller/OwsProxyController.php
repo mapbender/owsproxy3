c6bf8d2f1cd9bc046b032a07b05e6ee107c59cc5
werrolf <rolf.neuberger@wheregroup.com>
2021-08-27 14:50:07 +0200

    Remove unrouted (no url, internal subrequest only) genericProxyAction

--- src/OwsProxy3/CoreBundle/Controller/OwsProxyController.php
+++ src/OwsProxy3/CoreBundle/Controller/OwsProxyController.php
@@ -3,10 +3,7 @@ namespace OwsProxy3\CoreBundle\Controller;
 
 use Mapbender\CoreBundle\Component\Signer;
 use OwsProxy3\CoreBundle\Component\HttpFoundationClient;
-use OwsProxy3\CoreBundle\Component\Utils;
 use OwsProxy3\CoreBundle\Component\ProxyQuery;
-use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,57 +21,12 @@ class OwsProxyController
     protected $client;
     /** @var Signer */
     protected $signer;
-    /** @var LoggerInterface */
-    protected $logger;
 
     public function __construct(HttpFoundationClient $client,
-                                Signer $signer,
-                                LoggerInterface $logger = null)
+                                Signer $signer)
     {
         $this->client = $client;
         $this->signer = $signer;
-        $this->logger = $logger ?: new NullLogger();
-    }
-
-    /**
-     * Handles the client's request
-     * NOTE: no route; only reachable via Symfony internal SubRequest
-     * @deprecated for complex usage, uncontrollable query param merging and uncontrollable POST behaviour
-     * Use owsproxy.http_foundation_client service to internally retrieve Response objects from ProxyQuery objects (GET + POST)
-     * For Mapbender >= 3.0.8-beta1, you can also use getUrl method on mapbender.http_transport.service for simple url-based GET requests
-     * Use Utils:: methods to ease URL and post content construction.
-     *
-     * @see https://github.com/mapbender/mapbender/blob/v3.0.8.4/src/Mapbender/Component/Transport/OwsProxyTransport.php
-     *
-     * @param Request $request
-     * @param string $url
-     * @param string $content for POST
-     * @return Response
-     * @todo v3.3: remove
-     */
-    public function genericProxyAction(Request $request, $url, $content = null)
-    {
-        try {
-            $url = Utils::appendQueryParams($url, $request->query->all());
-            $headers = Utils::getHeadersFromRequest($request);
-            if (null === $content) {
-                $rq = $request->getContent();
-                if ($rq || $request->getMethod() === Request::METHOD_POST) {
-                    // force outgoing request to POST, even with empty body
-                    $content = $rq ?: '';
-                }
-            }
-            $content = Utils::extendPostContent($content, $request->request->all());
-            $url = Utils::filterDuplicateQueryParams($url, false);
-            if ($content !== null) {
-                $proxy_query = ProxyQuery::createPost($url, $content, $headers);
-            } else {
-                $proxy_query = ProxyQuery::createGet($url, $headers);
-            }
-        } catch (\InvalidArgumentException $e) {
-            throw new BadRequestHttpException($e->getMessage(), $e);
-        }
-        return $this->getQueryResponse($proxy_query, $request);
     }
 
     /**
